71a33201155f3b3f2d039f8c3d5168611d4676d6
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2021-03-30 23:49:35 +0600

    added slice and action

---
+++ src/redux/actions.js
@@ -0,0 +1,9 @@
+import {authSlice} from "./slice"
+const {actions : slice} = authSlice;
+
+
+
+// login actions
+export const loginAction = (phone) => (dispatch) => {
+    dispatch(slice.setLogin(phone))
+} 
